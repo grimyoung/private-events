540ec65c1445308c6c94fb25cd0f2c25c6a16689
grimyoung <samhyukim@gmail.com>
2021-01-07 20:42:36 -0800

    update user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,19 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
   end
 
   def create
+    @user = User.find(params[:id])
+    if @user.save
+      redirect_to root_url
+    else
+      render 'new'
+    end
   end
 
   def show
+    @user = User.find(params[:id])
+    
   end
 end
